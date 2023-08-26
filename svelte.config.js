3d5f65e7c9415048e8456f7916c57d79ab37c3e1
Mughees Khan <mugikhan@gmail.com>
2023-08-26 22:26:40 +0200

    remove source maps

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,10 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  compilerOptions: {
-    enableSourcemap: true,
-  },
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
-  },
-  build: {
-    sourcemap: true,
-  },
+  }
 };
 
 export default config;
