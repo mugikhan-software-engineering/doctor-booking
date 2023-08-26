e9868fce7b85f1c4c9dad087a9d41dbf58e5b162
Mughees Khan <mugikhan@gmail.com>
2023-08-26 22:17:33 +0200

    Moving to github secrets for api keys

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,9 @@
 import type { PageServerLoad } from './$types';
-import { env } from '$env/dynamic/private';
+import { PLACES_API_KEY } from '$env/static/private';
 
 export const load = (async () => {
 	const response = await fetch(
-		`https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJB3c16pyolR4RA3gLQl3Z5nI&key=${env.GOOGLE_API_KEY}`
+		`https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJB3c16pyolR4RA3gLQl3Z5nI&key=${PLACES_API_KEY}`
 	);
 	let res = await response.json();
 	return {
