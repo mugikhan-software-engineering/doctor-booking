59514ba51bceb6cd4e04234e1547780b7e26140a
Mughees Khan <mugikhan@gmail.com>
2023-09-05 22:11:14 +0200

    Update treatments page

---
+++ src/lib/constants/infertility.ts
@@ -0,0 +1,14 @@
+export const infertilities = [
+	{
+		title: 'Abnormal sperm production or function'
+	},
+	{
+		title: 'Problems with the delivery of sperm (Obstruction)'
+	},
+	{
+		title: 'Swollen veins in the scrotum (Varicoceles)'
+	},
+	{
+		title: 'Damage related to cancer and its treatment'
+	}
+] as const;
--- src/routes/treatments/+page.svelte
+++ src/routes/treatments/+page.svelte
@@ -6,9 +6,20 @@
 	import kidneyHome from '$lib/assets/kidney-home.jpg';
 	import kidneyHeader from '$lib/assets/kidney-header.jpg';
 	import bladderHeader from '$lib/assets/bladder-header.jpg';
+	import kidneysIcon from '$lib/assets/png/kidney.png';
+	import bladderIcon from '$lib/assets/png/bladder.png';
+	import prostateIcon from '$lib/assets/png/prostate.png';
+	import testicularIcon from '$lib/assets/png/testicular.png';
+	import infertilityIcon from '$lib/assets/png/infertility.png';
+	import sexualHealthIcon from '$lib/assets/png/sexual-health.png';
 
 	import { kidneys } from '$lib/constants/kidneys';
 	import { bladders } from '$lib/constants/bladder';
+	import { prostates } from '$lib/constants/prostate';
+	import { testiculars } from '$lib/constants/testicular';
+	import { infertilities } from '$lib/constants/infertility';
+
+	import { Avatar } from '@skeletonlabs/skeleton';
 
 	let isInViewKidneyTitle: boolean;
 	let isInViewBladderTitle: boolean;
@@ -34,116 +45,174 @@
 	</div>
 </section>
 
-<section id="kidney" class="flex flex-col h-fit w-screen mt-5">
-	<div
-		class="sticky flex bg-transparent px-0 md:p-2 mb-2 lg:flex-row flex-col items-center z-0 gap-y-8"
-	>
-		<div
-			class="flex flex-row flex-wrap w-full place-content-start items-start z-2"
-			use:inview
-			on:inview_change={(event) => {
-				const { inView } = event.detail;
-				isInViewKidneyTitle = inView;
-			}}
-		>
-			<svelte:component
-				this={floatingTitle}
-				title=""
-				subtitle="KIDNEYS."
-				yOffset={-50}
-				isVisible={isInViewKidneyTitle}
-			/>
-		</div>
-	</div>
+<div class="diseases flex flex-col">
+	<section id="kidney" class="flex flex-col h-fit w-screen bg-[#f4f5f7] py-8">
+		<div class="flex flex-row mt-5">
+			<div class="flex justify-center items-center basis-2/5">
+				<span class="bg-[#CBE9FA] w-44 h-44 rounded-full flex justify-center items-center">
+					<img src={kidneysIcon} class="w-36 h-36 rounded-full" alt="Kidneys" />
+				</span>
+			</div>
 
-	<div
-		class="sticky w-screen h-fit bg-cover bg-center bg-no-repeat bg-fixed bg-blend-color z-0"
-		style="background-image:url({kidneyHeader})"
-		use:inview
-	>
-		<div class="flex flex-row p-6">
-			<ul class="list card variant-glass-secondary p-6">
-				{#each kidneys as kidney, i}
-					<li class="text-white">
-						<span>
-							<svg
-								width="20px"
-								height="20px"
-								viewBox="0 0 24 24"
-								version="1.1"
-								xmlns="http://www.w3.org/2000/svg"
-								xmlns:xlink="http://www.w3.org/1999/xlink"
-							>
-								<g id="out" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-									<path
-										d="M19,3 L5,3 C3.9,3 3.01,3.9 3.01,5 L3,19 C3,20.1 3.9,21 5,21 L19,21 C20.1,21 21,20.1 21,19 L21,5 C21,3.9 20.1,3 19,3 L19,3 Z M18,14 L14,14 L14,18 L10,18 L10,14 L6,14 L6,10 L10,10 L10,6 L14,6 L14,10 L18,10 L18,14 L18,14 Z"
-										id="path"
-										fill="#FFF"
-									/>
-								</g>
-							</svg>
-						</span>
-						<span class="flex-auto py-2">{kidney.title}</span>
-					</li>
-				{/each}
-			</ul>
+			<div class="flex flex-col shrink-0 basis-3/5">
+				<div class="flex flex-row items-center">
+					<span class="badge-icon p-7 variant-soft-primary mr-0 md:mr-5 text-xl text-black"
+						>{1}</span
+					>
+					<h3 class="h2">Kidneys</h3>
+				</div>
+				<div>
+					<ul class="p-6 flex flex-col list-disc gap-y-1">
+						{#each kidneys as kidney}
+							<li class="text-black">
+								<span class="flex-auto">{kidney.title}</span>
+							</li>
+						{/each}
+					</ul>
+				</div>
+			</div>
 		</div>
-	</div>
-</section>
+	</section>
 
-<section id="bladder" class="flex flex-col h-fit w-screen mt-5">
-	<div
-		class="sticky flex bg-transparent px-0 md:p-2 mb-2 lg:flex-row flex-col items-center z-0 gap-y-8"
-	>
-		<div
-			class="flex flex-row flex-wrap w-full place-content-start items-start z-2"
-			use:inview
-			on:inview_change={(event) => {
-				const { inView } = event.detail;
-				isInViewBladderTitle = inView;
-			}}
-		>
-			<svelte:component
-				this={floatingTitle}
-				title=""
-				subtitle="BLADDERS."
-				yOffset={-50}
-				isVisible={isInViewBladderTitle}
-			/>
-		</div>
-	</div>
+	<section id="bladder" class="flex flex-col h-fit w-screen bg-[#FFF] py-8">
+		<div class="flex flex-row mt-5">
+			<div class="flex justify-center items-center basis-2/5">
+				<span class="bg-[#CBE9FA] w-44 h-44 rounded-full flex justify-center items-center">
+					<img src={bladderIcon} class="w-36 h-36 rounded-full" alt="Bladder" />
+				</span>
+			</div>
 
-	<div
-		class="sticky w-screen h-fit bg-cover bg-center bg-no-repeat bg-fixed z-0"
-		style="background-image:url({bladderHeader})"
-		use:inview
-	>
-		<div class="flex flex-row p-6">
-			<ul class="list card variant-glass-secondary p-6">
-				{#each bladders as bladder, i}
-					<li class="text-white">
-						<span>
-							<svg
-								width="20px"
-								height="20px"
-								viewBox="0 0 24 24"
-								version="1.1"
-								xmlns="http://www.w3.org/2000/svg"
-								xmlns:xlink="http://www.w3.org/1999/xlink"
-							>
-								<g id="out" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-									<path
-										d="M19,3 L5,3 C3.9,3 3.01,3.9 3.01,5 L3,19 C3,20.1 3.9,21 5,21 L19,21 C20.1,21 21,20.1 21,19 L21,5 C21,3.9 20.1,3 19,3 L19,3 Z M18,14 L14,14 L14,18 L10,18 L10,14 L6,14 L6,10 L10,10 L10,6 L14,6 L14,10 L18,10 L18,14 L18,14 Z"
-										id="path"
-										fill="#FFF"
-									/>
-								</g>
-							</svg>
-						</span>
-						<span class="flex-auto py-2">{bladder.title}</span>
-					</li>
-				{/each}
-			</ul>
+			<div class="flex flex-col shrink-0 basis-3/5">
+				<div class="flex flex-row items-center">
+					<span class="badge-icon p-7 variant-soft-primary mr-0 md:mr-5 text-xl text-black"
+						>{2}</span
+					>
+					<h3 class="h2">Bladders</h3>
+				</div>
+				<div>
+					<ul class="p-6 flex flex-col list-disc gap-y-1">
+						{#each bladders as bladder}
+							<li class="text-black">
+								<span class="flex-auto">{bladder.title}</span>
+							</li>
+						{/each}
+					</ul>
+				</div>
+			</div>
 		</div>
-	</div>
-</section>
+	</section>
+
+	<section id="prostate" class="flex flex-col h-fit w-screen bg-[#f4f5f7] py-8">
+		<div class="flex flex-row mt-5">
+			<div class="flex justify-center items-center basis-2/5">
+				<span class="bg-[#CBE9FA] w-44 h-44 rounded-full flex justify-center items-center">
+					<img src={prostateIcon} class="w-36 h-36 rounded-full" alt="Prostate" />
+				</span>
+			</div>
+
+			<div class="flex flex-col shrink-0 basis-3/5">
+				<div class="flex flex-row items-center">
+					<span class="badge-icon p-7 variant-soft-primary mr-0 md:mr-5 text-xl text-black"
+						>{3}</span
+					>
+					<h3 class="h2">Prostate</h3>
+				</div>
+				<div>
+					<ul class="p-6 flex flex-col list-disc gap-y-1">
+						{#each prostates as prostate}
+							<li class="text-black">
+								<span class="flex-auto">{prostate.title}</span>
+							</li>
+						{/each}
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="testicular" class="flex flex-col h-fit w-screen bg-[#FFF] py-8">
+		<div class="flex flex-row mt-5">
+			<div class="flex justify-center items-center basis-2/5">
+				<span class="bg-[#CBE9FA] w-44 h-44 rounded-full flex justify-center items-center">
+					<img src={testicularIcon} class="w-36 h-36 rounded-full" alt="Testicular" />
+				</span>
+			</div>
+
+			<div class="flex flex-col shrink-0 basis-3/5">
+				<div class="flex flex-row items-center">
+					<span class="badge-icon p-7 variant-soft-primary mr-0 md:mr-5 text-xl text-black"
+						>{4}</span
+					>
+					<h3 class="h2">Testicular</h3>
+				</div>
+				<div>
+					<ul class="p-6 flex flex-col list-disc gap-y-1">
+						{#each testiculars as testicular}
+							<li class="text-black">
+								<span class="flex-auto">{testicular.title}</span>
+							</li>
+						{/each}
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="infertility" class="flex flex-col h-fit w-screen bg-[#f4f5f7] py-8">
+		<div class="flex flex-row mt-5">
+			<div class="flex justify-center items-center basis-2/5">
+				<span class="bg-[#CBE9FA] w-44 h-44 rounded-full flex justify-center items-center">
+					<img src={infertilityIcon} class="w-36 h-36 rounded-full" alt="Infertility" />
+				</span>
+			</div>
+
+			<div class="flex flex-col shrink-0 basis-3/5">
+				<div class="flex flex-row items-center">
+					<span class="badge-icon p-7 variant-soft-primary mr-0 md:mr-5 text-xl text-black"
+						>{5}</span
+					>
+					<h3 class="h2">Male infertility</h3>
+				</div>
+				<div>
+					<ul class="p-6 flex flex-col list-disc gap-y-1">
+						{#each infertilities as infertility}
+							<li class="text-black">
+								<span class="flex-auto">{infertility.title}</span>
+							</li>
+						{/each}
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="sexual_health" class="flex flex-col h-fit w-screen bg-[#FFF] py-8">
+		<div class="flex flex-row mt-5">
+			<div class="flex justify-center items-center basis-2/5">
+				<span class="bg-[#CBE9FA] w-44 h-44 rounded-full flex justify-center items-center">
+					<img src={sexualHealthIcon} class="w-36 h-36 rounded-full" alt="Sexual health" />
+				</span>
+			</div>
+
+			<div class="flex flex-col shrink-0 basis-3/5">
+				<div class="flex flex-row items-center">
+					<span class="badge-icon p-7 variant-soft-primary mr-0 md:mr-5 text-xl text-black"
+						>{6}</span
+					>
+					<h3 class="h2">Male sexual health</h3>
+				</div>
+				<div>
+					<div class="p-6 flex flex-col gap-y-1">
+						<p class="text-black">
+							The male sexual dysfunction is the most common problem among males in our society
+							arising from various diseases including high blood pressure, diabetes, high blood
+							pressure treatment, smoking, high cholesterol, prostate cancer and enlargements,
+							testosterone deficiency, heart and vascular diseases (some psychological elements are
+							also responsible for this problem).
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
