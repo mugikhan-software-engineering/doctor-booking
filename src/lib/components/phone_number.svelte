613f6c5f7b53ad48aa6224f8d500e0f572a23a70
Mughees Khan <mugikhan@gmail.com>
2023-09-21 21:11:51 +0200

    Link styling

---
+++ src/lib/components/phone_number.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let number: string;
+	export let url: string;
+</script>
+
+<a
+	class="text-sm md:text-base text-primary-400 md:text-white sm:underline xs:underline underline-offset-2 md:no-underline link link-underline-thin link-underline-white py-[2px]"
+	href={url}
+>
+	{number}
+</a>
