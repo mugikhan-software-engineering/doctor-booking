4a4351697f4e17acafa128105ac6b10bee8f4fd9
Mughees Khan <mugikhan@gmail.com>
2025-05-01 22:31:11 +0200

    Default success message

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -66,7 +66,7 @@ export const actions: Actions = {
 
 			return {
 				success: true,
-				description: result.body
+				description: 'Your message has been sent successfully!'
 			};
 		} catch (err) {
 			return fail(400, {
