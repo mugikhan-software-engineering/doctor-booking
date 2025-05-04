5b64f7d0ab58dc3f35e38fdee5ddc61b02bc30ef
Mughees Khan <mugikhan@gmail.com>
2025-05-04 18:10:41 +0200

    Quick nav styling

---
+++ src/lib/components/links/quick_nav_link.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import ChevronsDownIcon from '~icons/mdi/chevron-double-down'
+    let { scrollIntoView, text, href }: { scrollIntoView: (event: Event) => void, text: string, href: string } = $props();
+</script>
+
+<div class="flex flex-row items-center">
+    <a
+        class="text-sm md:text-lg text-white text-shadow-sm"
+        href={href}
+        onclick={scrollIntoView}>{text}</a
+    >
+    <ChevronsDownIcon style="width: 24px; height: 24px;" class="text-white" />
+</div>
